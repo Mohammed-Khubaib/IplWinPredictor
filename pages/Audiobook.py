0d75845b7f90b053b4f6596f76e9cc24617409ea
Khubaib <mohmmedkhubaib2001@gmail.com>
2023-04-12 16:25:52 +0530

    tring to use the AudioBook

--- pages/Audiobook.py
+++ pages/Audiobook.py
@@ -2,19 +2,25 @@ import pyttsx3
 import PyPDF2
 import os
 import streamlit as st
+import tempfile
+
 st.title('My AudioBook')
 file = st.file_uploader("Pick a file",type="pdf")
-book = open(file, 'rb')
-pdfReader = PyPDF2.PdfReader(book)
-pages = len(pdfReader.pages)
+if file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(file.read())
+        book = open(tmp_file.name, 'rb')
 
-speaker = pyttsx3.init()
-s = st.number_input("Starting PageNo.")
-e = st.number_input("ending PageNo.")
-button = st.button('Press to start')
-if button:
-    for num in range(s, e):
-        page = pdfReader.pages[num]
-        text = page.extract_text()
-        speaker.say(text)
-        speaker.runAndWait()
+    pdfReader = PyPDF2.PdfFileReader(book)
+    pages = pdfReader.getNumPages()
+
+    speaker = pyttsx3.init()
+    s = st.number_input("Starting PageNo.")
+    e = st.number_input("ending PageNo.")
+    button = st.button('Press to start')
+    if button:
+        for num in range(s-1, e):
+            page = pdfReader.pages[num]
+            text = page.extractText()
+            speaker.say(text)
+            speaker.runAndWait()
